d65e5f6ba6071cbbbe86b7d0a3419bff33d5746f
mbertis <madisonbertis@gmail.com>
2020-10-20 13:32:42 -0700

    added objects to pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
-let pokemonList = [];
+let pokemonList = [
+    {name: 'Bulbasaur', height: 2, type: ['grass', 'poison']},
+    {name: 'Charizard', height: 5, type: ['fire', 'flying']},
+    {name: 'Torterra', height: 7, type: ['grass', 'ground']},
+    {name: 'Morgrem', height: 2, type: ['dark', 'fairy']}
+];
