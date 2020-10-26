65f36728ab2d71c4e670778e76939256d5207674
mbertis <madisonbertis@gmail.com>
2020-10-26 15:03:03 -0700

    placed pokemonList array into IIFE and called array through IIFE functions in forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,10 @@
-let pokemonList = [
+let pokemonRepository = (function() {
+  let pokemonList = [
   { name: "Bulbasaur", height: 2, type: ["grass", "poison"] },
   { name: "Charizard", height: 5, type: ["fire", "flying"] },
   { name: "Torterra", height: 7, type: ["water", "ground"] },
   { name: "Morgrem", height: 2, type: ["dark", "fairy"] },
-];
-
-let pokemonRepository = (function() {
-  let pokemonList = [];
+  ];
 
   return {
     add: function(pokemon) {
@@ -18,7 +16,7 @@ let pokemonRepository = (function() {
   }
 }) ();
 
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
   let size = "";
   if (pokemon.height > 6) {
     size = " This is a big Pokemon!";
