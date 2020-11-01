8949d99bfeeec143df4afcbd9052b305a47c57c6
mbertis <madisonbertis@gmail.com>
2020-10-31 17:45:10 -0700

    reverted changes to scripts file to attempt adding modal again

--- js/scripts.js
+++ js/scripts.js
@@ -11,56 +11,11 @@ let pokemonRepository = (function () {
     return repository;
   }
 
-  function showModal(title, text) {
-    modalContainer.innerHTML = "";
-    let modal = document.createElement('div');
-    modal.classList.add('modal');
-
-    let closeButtonElement = document.createElement('button');
-    closeButtonElement.classList.add('modal-close');
-    closeButtonElement.innerText = "Close";
-    closeButtonElement.addEventListener('click', hideModal);
-
-    let titleElement = document.createElement('h1');
-    titleElement.innerText = title;
-
-    let contentElement = document.createElement('p');
-    contentElement.innerText = text;
-
-    modal.appendChild(closeButtonElement);
-    modal.appendChild(titleElement);
-    modal.appendChild(contentElement);
-    modalContainer.appendChild(modal);
-
-    modalContainer.classList.add('is-visible');
-  }
-
-  document.querySelector('pokemon-button').addEventListener('click', () => {
-    showModal(pokemon.name, pokemon.height);
-  });
-  
-  function hideModal() {
-    modalContainer.classList.remove('is-visible');
-  }
-
-  window.addEventListener('keydown', (e) => {
-    if(e.key === "Escape" && modalContainer.classList.contains('is-visible')) {
-      hideModal();
-    }
-  });
-
-  modalContainer.addEventListener('click', (e) => {
-    let target = e.target;
-    if(target === modalContainer) {
-      hideModal();
-    }
-  });
-
-  function showDetails(pokemon) {
-    loadDetails(pokemon).then(function () {
-      
-    });
-  }
+    function showDetails(pokemon) {
+     loadDetails(pokemon).then(function () {
+       console.log(pokemon);
+     });
+   }
 
   function addListItem(pokemon) {
     let pokemonList = document.querySelector(".pokemon-list");
@@ -117,8 +72,8 @@ let pokemonRepository = (function () {
     addListItem: addListItem,
     loadList: loadList,
     loadDetails: loadDetails,
-    showModal: showModal,
-    hideModal: hideModal
+    //showModal: showModal,
+    //hideModal: hideModal
   };
 })();
 
