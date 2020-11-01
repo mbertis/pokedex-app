0695c5a295560eebbc822bcdfe23538418bd48cb
mbertis <madisonbertis@gmail.com>
2020-11-01 11:30:12 -0800

    added dynamic src for pokemon image in modal

--- js/scripts.js
+++ js/scripts.js
@@ -28,9 +28,13 @@ let pokemonRepository = (function () {
        let contentElement = document.createElement('p');
        contentElement.innerText = "Height: " + pokemon.height;
 
+       let imageElement = document.createElement('img');
+       imageElement.src = pokemon.imageUrl;
+
        modal.appendChild(closeButtonElement);
        modal.appendChild(titleElement);
        modal.appendChild(contentElement);
+       modal.appendChild(imageElement);
        modalContainer.appendChild(modal); // appends modal to modalContainer element
 
        modalContainer.classList.add("is-visible"); // allows modal-container to be visible
