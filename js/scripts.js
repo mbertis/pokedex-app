33f9b37fd58f92a79cec2d305c8e38d7a7be565a
mbertis <madisonbertis@gmail.com>
2020-11-05 15:05:36 -0800

    formatted scripts file with prettier

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonRepository = (function () {
   let repository = [];
   let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
- 
+
   function add(pokemon) {
     return repository.push(pokemon);
   }
@@ -12,24 +12,23 @@ let pokemonRepository = (function () {
 
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
-    
-    let modalBody = $(".modal-body");
-    let modalTitle = $(".modal-title");
-    
-    modalTitle.empty();
-    modalBody.empty();
+      let modalBody = $(".modal-body");
+      let modalTitle = $(".modal-title");
 
-    let nameElement = $("<h1>" + pokemon.name + "<h1>");
+      modalTitle.empty();
+      modalBody.empty();
 
-    let imageElement = $("<img class = 'modal-image' style = 'width:38%'>"); // dynamically displays pokemon image
-    imageElement.attr("src", pokemon.imageUrl);
+      let nameElement = $("<h1>" + pokemon.name + "<h1>");
 
-    let heightElement = $("<p>" + "Height: " + pokemon.height + "</p>");
+      let imageElement = $("<img class = 'modal-image' style = 'width:38%'>"); // dynamically displays pokemon image
+      imageElement.attr("src", pokemon.imageUrl);
 
-    modalTitle.append(nameElement);
-    modalBody.append(imageElement);
-    modalBody.append(heightElement);
-   });
+      let heightElement = $("<p>" + "Height: " + pokemon.height + "</p>");
+
+      modalTitle.append(nameElement);
+      modalBody.append(imageElement);
+      modalBody.append(heightElement);
+    });
   }
 
   function addListItem(pokemon) {
@@ -45,8 +44,8 @@ let pokemonRepository = (function () {
     pokemonList.appendChild(listItem);
     button.addEventListener("click", function () {
       showDetails(pokemon); // this will allow the modal to be displayed when pokemon button is clicked
-  });
-}
+    });
+  }
 
   function loadList() {
     return fetch(apiUrl)
