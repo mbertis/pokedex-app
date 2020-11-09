a88c79f73d8fa32fa4f7f421068c98150daa49b0
mbertis <madisonbertis@gmail.com>
2020-11-09 14:39:21 -0800

    added search function

--- js/scripts.js
+++ js/scripts.js
@@ -97,3 +97,20 @@ pokemonRepository.loadList().then(function () {
     pokemonRepository.addListItem(pokemon);
   });
 });
+
+function search() {
+  let input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.querySelectorAll(".list");
+  for(i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("button")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "No Pokemon Found";
+    }
+  }
+}
